90e584e28636614bbe2f9d8e4043413f73e42e9b
gaofeng <gaofeng@qq.com>
2018-09-28 23:35:37 +0800

    上传了项目

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,64 @@ const connection = require('./conn');
 
 // 调用连接方法
 connection.connect(() => {
-  console.log('数据库连接成功!');  
+  console.log('数据库连接成功!');
 });
+
+/* 检查用户名和密码 */
+router.post('/checklogin', (req, res) => {
+  // 接受用户和密码
+  let {username, password} = req.body;
+
+  // 构造sql 
+  const sqlStr = `select * from users where username='${username}' and password='${password}'`;
+ 
+  // 执行sql语句 查询用户名和密码是否存在
+  connection.query(sqlStr, (err,data) => {
+    if(err){
+      throw err;
+    }else{
+      // 如果存在 返回成功的数据对象
+      if(data.length){
+        // 登录成功 设置cookie
+        res.cookie('username', data[0].username);
+        res.cookie('password', data[0].password);
+        res.cookie('groups', data[0].groups);
+
+        res.send({'errcode': 1, 'msg': '恭喜你，登录成功！'});
+      }else{
+        res.send({'errcode': 0, 'msg': '请检查用户名和密码！'});
+      }
+    }
+  })
+})
+
+/* 检测用户是否登录 */
+router.get('/checkIsLogin', (req, res) => {
+  // 从浏览器获取cookie
+  let username = req.cookies.username;
+  // 如果存在 响应一下
+  if (username) {
+    res.send('console.log("")')
+  } else {
+    // 否则 弹出提示 跳到登录页面
+    res.send('alert("请登录以后再操作"); location.href="./login.html";')
+  }
+})
+
+/* 退出登录 */
+router.get('/loginout', (req, res) => {
+  // 清除cookie
+  res.clearCookie('username');
+  res.clearCookie('password');
+  res.clearCookie('groups');
+
+  res.send('<script>alert("欢迎下次再来！"); location.href="http://localhost:999/login.html"</script>')
+})
 
 /* 接收添加用户账号的请求 */
 router.post('/userAdd', (req, res) => {
-  // 接收前端用户账号的数据
-  // let {username, password, group} = req.body;
-  /*console.log('接收到的数据:',username, password, group)  // 这里要测试 */
+
+  let { username, password, group } = req.body;
 
   // 构造sql语句
   const sqlStr = 'insert into users(username, password, groups) values(?,?,?)';
@@ -30,15 +80,14 @@ router.post('/userAdd', (req, res) => {
       // 判断 如果受影响的行数 > 0 就是插入成功了
       if (data.affectedRows > 0) {
         // 响应一个成功的数据对象回去
-        res.send({"errcode": 1, "msg":"添加成功!"})
+        res.send({ "errcode": 1, "msg": "添加成功!" })
       } else {
         // 否则 就是插入失败 响应一个失败的数据对象回去
-        res.send({"errcode": 0, "msg":"添加失败!"})
+        res.send({ "errcode": 0, "msg": "添加失败!" })
       }
     }
   })
 });
-
 
 /* 接收显示所有用户账号的请求 */
 router.get('/userList', (req, res) => {
@@ -60,7 +109,7 @@ router.get('/userList', (req, res) => {
 /* 接收单条删除的请求 */
 router.get('/userDeleteOne', (req, res) => {
   // 接收前端发送过来的id
-  // let {id} = req.query;
+  let { id } = req.query;
 
   // 构造根据id删除指定数据的sql语句
   const sqlStr = `delete from users where id = ${id}`;
@@ -73,15 +122,83 @@ router.get('/userDeleteOne', (req, res) => {
     } else {
       // 判断 如果受影响的行数 > 0 返回给前端删除成功的数据对象
       if (data.affectedRows > 0) {
-        res.send({"errcode":1, "msg":"删除成功！"})
+        res.send({ "errcode": 1, "msg": "删除成功！" })
       } else {
         // 否则 返回给前端删除失败的数据对象
-        res.send({"errcode":0, "msg":"删除失败！"})
+        res.send({ "errcode": 0, "msg": "删除失败！" })
       }
     }
   })
 })
 
+/* 修改用户账号 把原来数据查出来给前端 */
+router.get('/useredit', (req, res) => {
+  // 接收id
+  let { id } = req.query;
+  // 根据id 构造sql查询语句
+  const sqlStr = `select * from users where id=${id}`;
 
+  // 执行sql语句
+  connection.query(sqlStr, (err, data) => {
+    // 如果有错 抛出错误
+    if (err) {
+      throw err;
+    } else {
+      res.send(data);
+    }
+  })
+})
+
+/* 接收用户账号 用修改的数据覆盖原来的数据 */
+router.post('/edit', (req, res) => {
+  // 接收前端发过来的数据
+  let { username, password, group, id } = req.body;
+
+  // 构造sql语句
+  const sqlStr = `update users set username='${username}',password='${password}',groups='${group}' where id=${id}`;
+
+  // 执行sql修改语句
+  connection.query(sqlStr, (err, data) => {
+    // 如果有错 抛出错误
+    if (err) {
+      throw err;
+    } else {
+      // 判断 如果受影响的行数 > 0 返回给前端删除成功的数据对象
+      if (data.affectedRows > 0) {
+        res.send({ "errcode": 1, "msg": "修改成功！" })
+      } else {
+        // 否则 返回给前端删除失败的数据对象
+        res.send({ "errcode": 0, "msg": "修改失败！" })
+      }
+    }
+  })
+})
+
+/* 接收批量删除的数组 */
+router.post('/batchesdel', (req, res) => {
+  // 接收前端选中的数据 批量删除
+  let idArr = req.body['idArr[]']
+
+  // 构造sql 根据id删除选中的数据全部被干掉
+  const sqlStr = `delete from users where id in (${idArr})`;
+
+  // 执行sql语句
+  connection.query(sqlStr, (err, data) => {
+    // 如有有错 抛出错误
+    if (err) {
+      throw err;
+    } else {
+      // 判断 如果受影响的行数 > 0 就是插入成功了
+      if (data.affectedRows > 0) {
+        // 响应一个成功的数据对象回去
+        res.send({ "errcode": 1, "msg": "批量删除成功!" })
+      } else {
+        // 否则 就是插入失败 响应一个失败的数据对象回去
+        res.send({ "errcode": 0, "msg": "批量删除失败!" })
+      }
+    }
+  })
+  
+})
 
 module.exports = router;
